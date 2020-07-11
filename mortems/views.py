88fd0122c015205137256ee89820aeba83f1ebe1
Pete Naveen Pearson <coffee.drinker.daniel@gmail.com>
2020-07-10 20:37:35 -0400

    Revisions based on Manjit Kumar's feedback on SO

--- mortems/views.py
+++ mortems/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render, get_object_or_404
 from mortems.models import Mortem
 
 def mortems(request):
-    mortem = Mortem.objects.all().order_by('-pub_date')
-    context = {'mortem':mortem}
+    mortems = Mortem.objects.all().order_by('-pub_date')
+    context = {'mortems':mortems}
     return render(request, 'alls/landings.html', context)
 
