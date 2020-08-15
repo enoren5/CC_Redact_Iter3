ddc4bca8f9a2fa639be366b954c8586d60a78d36
Pete Naveen Pearson <coffee.drinker.daniel@gmail.com>
2020-08-15 06:38:49 -0400

    My SO question is answered. More details: https://stackoverflow.com/questions/63357891/django-serving-wrong-template-at-the-wrong-address-with-malformed-urls-py-redac

--- CC_Redact_Iter3/urls.py
+++ CC_Redact_Iter3/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('posts.urls')),
     path('', include('redactors.urls')),
+    path('', include('posts.urls')),    
     path('', include('counters.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- redactors/views.py
+++ redactors/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+# from posts.views import context
+from posts.models import Posts
 
-# Create your views here.
 def redactors(request):
     return render(request, 'alls/landings.html')
 
@@ -8,7 +9,9 @@ def home(request):
     if 'ccEntry' in request.GET:
         number = request.GET['ccEntry']
         redacted_num = 'xxxx xxxx {}'.format(number[-4:])
-        return render(request, 'alls/landings.html', {'number':number, 'redacted_num':redacted_num})
+        posts = Posts.objects.all().order_by('-pub_date')
+        # context = {'posts':posts}
+        return render(request, 'alls/landings.html', {'number':number, 'redacted_num':redacted_num, 'posts':posts, })
     else:
         return render(request, 'alls/landings.html')
 
