6b32765c349fd19b0ed85cfb86fe5a023949c045
Pete Naveen Pearson <coffee.drinker.daniel@gmail.com>
2020-10-02 03:51:38 -0400

    Plumbing mile2km feature, it works!

--- mile2km/views.py
+++ mile2km/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
 def mile2km(request):
-    Mile = input("Enter the number of miles to convert to kilometers")
-    Mile = float(Mile)
-    KMratio = float(1.60934)
-    KM = Mile * KMratio
-    context = None
-    return render(request, 'alls/mile2km.html', context) 
+    if 'mile2km' in request.GET:
+        mile = request.GET['mile2km']
+        mile = float(mile) # cast mile value to float
+        ratio = float(1.60934) 
+        km = mile * ratio # output defined
+        context = {"km":km}
+        return render(request, 'alls/results.html', context)
+    else:
+        return render(request, 'alls/mile2km.html') 
+
+    # collect mile input from client somehow
+    
+    
