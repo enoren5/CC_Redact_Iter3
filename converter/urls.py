1c440cac0fb05662891b0df9834cee8644db1691
Pete Naveen Pearson <coffee.drinker.daniel@gmail.com>
2020-10-02 04:38:07 -0400

    Minor refactor

--- converter/urls.py
+++ converter/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('converter', views.mile2km, name='mile2km'),
-    path('converter', views.km2mile, name='km2mile'),
+    path('converter', views.mile2km, name='converter'),
+    path('converter', views.km2mile, name='converter'),
 ]
