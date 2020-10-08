7d327308d3939c7056d07ee503775f700cce30d0
UmarGit <umarsheikh303@gmail.com>
2020-10-08 05:28:44 +0500

    Moved top 10 counting
    Moved top 10 counting and using merged alice and blog data.

--- wordcounters/views.py
+++ wordcounters/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
 from collections import Counter
-import re
+from blogs.models import Posts
 from nltk.corpus import stopwords
+from blogs.utils import top_word_counts
 
 def wordcounters(request, text=""):
     """
     This function processes the top 10 most common words in a text file and then renders them.
     """
-    text = open("wordcounters/Alice.txt", "r").read().lower()
-    stoplist = stopwords.words('english')
-    stoplist.extend(["said","gutenberg", "could", "would",]) 
-    clean = []
-    for word in re.split(r"\W+",text):
-        if word not in stoplist:
-            clean.append(word)
-    top_10 = Counter(clean).most_common(10)
+    posts = Posts.objects.all().order_by('-pub_date')
+
+    post_string = ''
+    for post in posts:
+        post_string += post.body
+
+    post_text = post_string.lower()
+
+    alice_text = open("wordcounters/Alice.txt", "r").read().lower()
+
+    top_10 = top_word_counts(post_text + alice_text)
+
     return render(request, 'alls/landings.html', {'top_10': top_10})
