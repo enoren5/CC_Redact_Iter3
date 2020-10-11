5730e6ee98b6b829a39e4f0c81e0a5c16298bf8f
UmarGit <umarsheikh303@gmail.com>
2020-10-11 05:08:14 +0500

    Update move-top-10-counting
    Update move-top-10-counting

--- blogs/views.py
+++ blogs/views.py
@@ -7,6 +7,12 @@ from wordcounters.views import wordcounters
 def posts(request):
     # Based on Pankaj Mishra's SO answere here: https://stackoverflow.com/a/45411928/6095646
     # This is Pankaj Mishra's hit counter:
+    context = BlogWordsCount(request)
+
+    return render(request, 'alls/landings.html', context)
+
+def BlogWordsCount(request):
+
     if (PageView.objects.count()<=0):
         x = PageView.objects.create()
         x.save()
@@ -15,11 +21,12 @@ def posts(request):
         x.hits = x.hits+1
         x.save()
     context = {'pages':x.hits}
-
+    
+    # The posts content:
     posts = Posts.objects.all().order_by('-pub_date')
     context.update({'posts':posts})
 
     context.update(wordcounters(request))
 
-    return render(request, 'alls/landings.html', context)
+    return context
     
--- wordcounters/views.py
+++ wordcounters/views.py
@@ -8,6 +8,7 @@ def wordcounters(request, text=""):
 
     post_words = top_word_counts(post_text)
     alice_words = top_word_counts(alice_text)
+    
     context = ({
         'post_words': post_words,
         'alice_words': alice_words
