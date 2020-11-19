674dce34e1b30a5599fc4eeaba419eaa9bc50e55
Pete Naveen Pearson <coffee.drinker.daniel@gmail.com>
2020-11-19 13:55:16 -0500

    Implement syntax-highlighting

--- CC_Redact_Iter3/settings.py
+++ CC_Redact_Iter3/settings.py
@@ -71,6 +71,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            # 'libraries':{
+            #    'markdownify': 'app.templatetags.markdownify',
+            #}
         },
     },
 ]
